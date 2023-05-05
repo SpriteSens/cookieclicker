5ad597adf26a551a0e7620df07ab59e74641c205
SpriteSens <86854233+SpriteSens@users.noreply.github.com>
2023-05-05 16:50:10 -0500

    Updated main.js
    no more wrong address

--- main.js
+++ main.js
@@ -15919,9 +15919,9 @@ window.onload=function()
 				LoadLang('loc/'+lang+'.js?v='+Game.version,function(){
 					var launch=function(){
 						Game.Launch();
-						if (top!=self) Game.ErrorFrame();
-						else
-						{
+						 (top!=self);
+						
+						
 							console.log('[=== '+choose([
 								'Oh, hello!',
 								'hey, how\'s it hangin',
